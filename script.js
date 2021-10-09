929053caf38d67eb6826534e2b69dccafbf74a2b
Aazib Chaudhry <aazibch47@gmail.com>
2021-10-09 16:14:53 +0500

    Remove unnecessary comments

--- script.js
+++ script.js
@@ -70,71 +70,37 @@ $(document).ready(() => {
       const enteredValue = $(this).attr('value');
 
       /* Push new value */
-
       if (
-        // No input,
-        // And plus or minus operators are entered
         (inputs.length === 0 && mathOperators.indexOf(enteredValue) >= 2) ||
-        // Some input
-        // And the last input isn't an operator
         (inputs.length > 0 &&
           mathOperators.indexOf(inputs[inputs.length - 1]) === -1) ||
-        // Some input
-        // The entered value is "-"
-        // And the last input is an operator
-        // And the second last input is not an operator
         (inputs.length > 0 &&
           enteredValue === '-' &&
           mathOperators.indexOf(inputs[inputs.length - 1]) !== -1 &&
           mathOperators.indexOf(inputs[inputs.length - 2]) === -1)
       ) {
         inputs.push(enteredValue);
-      } else if (
-        /* Replace Values */
-        // Some input
-        // and the last value is an operator
-        // and the second last value is also an operator
+      }
+      // Replace last two inputs
+      else if (
         inputs.length > 0 &&
         mathOperators.indexOf(inputs[inputs.length - 1]) !== -1 &&
         mathOperators.indexOf(inputs[inputs.length - 2]) !== -1
       ) {
         inputs.splice(inputs.length - 2, 2);
         inputs.push(enteredValue);
-      } else if (
-        // There is one input,
-        // It is a math operator (plus or minus)
-        // And the entered value is either plus or minus operator
+      }
+      // Replace last input
+      else if (
         (inputs.length === 1 &&
           mathOperators.indexOf(inputs[inputs.length - 1]) !== -1 &&
           mathOperators.indexOf(enteredValue) >= 2) ||
-        // There is input
-        // The entered value is not the minus sign
-        // And the last value is a math operator
         (inputs.length > 0 &&
           enteredValue !== '-' &&
           mathOperators.indexOf(inputs[inputs.length - 1]) !== -1)
       ) {
-        // replace last input value
         inputs[inputs.length - 1] = enteredValue;
       }
-
-      // if (
-      //   // No input yet,
-      //   // and plus or minus sign in added.
-      //   (inputs.length === 0 && mathOperators.indexOf(enteredValue) >= 2) ||
-      //   // Input is present,
-      //   // and the last input is not a mathematical operator
-      //   (inputs.length > 0 &&
-      //     mathOperators.indexOf(inputs[inputs.length - 1]) === -1) ||
-      //   // Input is present,
-      //   // and the entered operator is either plus or minus
-      //   // and the second last input is not a math operator.
-      //   (inputs.length > 1 &&
-      //     mathOperators.indexOf(enteredValue) >= 2 &&
-      //     mathOperators.indexOf(inputs[inputs.length - 2]) === -1)
-      // ) {
-      //   inputs.push(enteredValue);
-      // }
 
       if (inputs.length > 0) {
         updateScreen(inputs.join(''));
